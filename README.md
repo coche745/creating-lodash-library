52a2d692232df9540b17c2706542d4e383be86c6
Coche <70551980+coche745@users.noreply.github.com>
2021-03-31 19:07:43 -0500

    update readme and explanation of methods

---
+++ README.md
@@ -0,0 +1,14 @@
+# Creating the Lodash Library
+## Table of Contents
++ General Info
++ Technologies
++ Setup
+## General Info
+Implementation of some of the most exciting funcionality from the widely-popular lodash.js library which provides many methods that add new functionality for numbers, strings, objects, and arrays.
+
+I created this project as part of Codecademy's curriculum on my own computer in order to put into practice my knowledge of objects and their methods.
+## Technologies
++ Javascript
+## Setup
+To run this project, install it locally using git
++ git clone https://github.com/coche745/creating-lodash-library
--- _.js
+++ _.js
@@ -1,12 +1,5 @@
 const _ = {
     clamp (num, lower, upper) {
-        /*if (num < lower) {
-            return lower;
-        } else if (lower <= num <= upper) {
-            return num;
-        } else if (num > upper) {
-            return upper;
-        }*/
         let value = Math.max(num, lower);
         value = Math.min(value, upper);
         return value;
@@ -52,13 +45,6 @@ const _ = {
           }
       },
       invert (obj) {
-          /*keyArr = Object.keys(obj);
-          valueArr = Object.values(obj);
-          obj = {};
-          for (let i = 0; i < keyArr.length; i++) {
-              obj[valueArr[i]] = keyArr[i];
-          }
-          return obj;*/
           let newObj = {}
           for (let i in obj) {
               newObj[obj[i]] = i;
@@ -115,9 +101,66 @@ const _ = {
           return newArr;
       }
   };
-  
 
-console.log(_.chunk([1, 2, 3, 4, 5], 2));
+// erase the // before each console.log to see how each method of our library works!
+  
+/* clamp takes in a number, an upper, and a lower bound. Returns lower if number less than lower. Returns upper
+if number greater than upper. Returns number as is if number between upper and lower. */
+    console.log(_.clamp(5, 6, 9));
+    console.log(_.clamp(7, 6, 9));
+
+/* inRange takes in a number, a start value, and an end value. Returns true if number between start and end.
+Returns false if number less than start or greater than or equal to end. If start is greater than end, start
+and end are swapped. */
+    // console.log(_.inRange(4, 3, 8));
+
+/* words takes in a string and creates an array of the words from that string. */
+    //console.log(_.words('This is a sentence'));
+
+/* pad takes in a string and a number, and adds spaces to the beginning and end of the string until the amount
+of characters in the string matches the number */
+    // console.log(_.pad('hello', 8));
+    // console.log(_.pad('hello', 15));
+
+/* has takes in an object and a key and checks to see whether the object has a value at that key. Returns true 
+if there is a value at the key and false if there is not */
+    let obj = {1: 'weightlifting', 2: 'cardio', 3: 'calisthenics', 4: ''};
+    // console.log(_.has(obj, 3));
+    // console.log(_.has(obj, 4));
+
+/* invert takes in an object and swaps each key with its value. */
+    // console.log(_.invert(obj));
+
+/* findKey takes in an object and a function and calls the function on each value of the object. findKey 
+returns the first key that contains a value that makes the function return true. If the function returns false 
+for all the values in the object, then findKey will return undefined. */
+    let cardioFinder = (str) => {
+        return str === 'cardio';
+    }
+    let powerliftingFinder = (str) => {
+        return str === 'powerlifting';
+    }
+    // console.log(_.findKey(obj, cardioFinder));
+    // console.log(_.findKey(obj, powerliftingFinder));
+
+/* drop takes in an array and a number, and takes out from the beginning of the array the amount of elements 
+specified by number. If no number is supplied, number = 1*/
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    // console.log(_.drop(arr, 4));
+
+/* dropWhile takes in an array and a function, and calls the function on each element of the array. dropWhile 
+drops elements from the beginning of the array until the function returns false, then returns the remaining 
+array*/
+
+    let myFunction = (i, index, arr) => {
+        return i < 7;
+    }
+    // console.log(_.dropWhile(arr, myFunction));
+
+/* chunk takes in an array and a size and breaks up the array into arrays of the specified size. chunk returns 
+and array of these array chunks. If the array can't be broken up evenly, the last array chunk will be smaller 
+than the specified size. If no size is supplied, size = 1. */
+    // console.log(_.chunk([1, 2, 3, 4, 5], 2));
 
  
 
